daed4cd639007586490492770cbea87ec7bceff5
Gunjan0031 <81515950+Gunjan0031@users.noreply.github.com>
2023-04-25 14:30:10 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,11 +5,8 @@ export default function About() {
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-        repudiandae architecto qui adipisci in officiis, aperiam sequi atque
-        perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur
-        nobis veritatis quasi explicabo obcaecati doloremque? Placeat ratione
-        hic aspernatur error blanditiis?
+        This website Drinkade is designed to search different types of drinks available online. 
+    It is using external API to fetch different types of drink names available.
       </p>
     </section>
   );
